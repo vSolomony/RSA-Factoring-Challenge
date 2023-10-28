82246b67bfe48541543160900ead81a39195e96f
vSolomony <mohamed.a.kassem95@gmail.com>
2023-10-28 11:24:25 -0700

    p

---
+++ factors.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def factorize(n):
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            return i, n // i
+    return 1, n
+
+
+def main(filename):
+    try:
+        with open(filename, "r") as file:
+            lines = file.read().splitlines()
+            for line in lines:
+                n = int(line)
+                p, q = factorize(n)
+                print(f"{n}={p}*{q}")
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python factors.py <file>")
+    else:
+        filename = sys.argv[1]
+        main(filename)
